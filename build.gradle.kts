41cbc85a34bb0c8978e4ac06d5d6b4e613a93ca2
kateLee <xoolwater@gmail.com>
2021-06-06 15:46:15 +0800

    Add JSON Lib: Gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
--- src/main/kotlin/kate/tutorial/kotlin/plugins/Routing.kt
+++ src/main/kotlin/kate/tutorial/kotlin/plugins/Routing.kt
@@ -3,14 +3,20 @@ package kate.tutorial.kotlin.plugins
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
 
 fun Application.configureRouting() {
     // Starting point for a Ktor app:
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(mapOf("message" to "HELLO WORLD!"))
         }
     }
 
