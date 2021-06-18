36eb859ed3c867050a7f444c1ad363ea8a553d38
kateLee <xoolwater@gmail.com>
2021-06-18 18:14:06 +0800

    Mock puzzles random data

--- src/main/kotlin/kate/tutorial/kotlin/plugins/Routing.kt
+++ src/main/kotlin/kate/tutorial/kotlin/plugins/Routing.kt
@@ -7,6 +7,9 @@ import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import kate.tutorial.kotlin.puzzle.PuzzleResponse
+import java.util.*
+import kotlin.collections.ArrayList
 
 fun Application.configureRouting() {
     // Starting point for a Ktor app:
@@ -18,10 +21,23 @@ fun Application.configureRouting() {
         get("/") {
             call.respond(mapOf("message" to "HELLO WORLD!"))
         }
-        get("/api/puzzles/{id}") {
-            TODO()
+        get("/api/puzzles") {
+            val list = ArrayList<PuzzleResponse>()
+            for (i in 0..10) {
+                list.add(
+                    PuzzleResponse(
+                        id = UUID.randomUUID(),
+                        title = "從前從前有碗" + listOf("海龜湯", "孟婆湯", "玉米湯", "南瓜湯").random(),
+                        avatar = "https://imgur.com/l0swFL1.jpg",
+                        attendance = (0..10).random().toString() + "人")
+                )
+            }
+            call.respond(list)
         }
         post("/api/puzzles") {
+            TODO()
+        }
+        get("/api/puzzles/{id}") {
             TODO()
         }
         delete("/api/puzzles/{id}") {
---
+++ src/main/kotlin/kate/tutorial/kotlin/puzzle/PuzzleResponse.kt
@@ -0,0 +1,10 @@
+package kate.tutorial.kotlin.puzzle
+
+import java.util.*
+
+data class PuzzleResponse (
+    val id: UUID,
+    val avatar: String,
+    val title: String,
+    val attendance: String,
+)
