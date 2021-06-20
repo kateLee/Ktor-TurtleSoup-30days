f0201627a831df893402dbf989bbb932d8678f1c
kateLee <xoolwater@gmail.com>
2021-06-20 19:07:16 +0800

    Add Lib: exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+val exposed_version: String by project
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -23,4 +24,10 @@ dependencies {
     implementation("io.ktor:ktor-gson:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
+    implementation("com.h2database:h2:1.4.200")
 }
